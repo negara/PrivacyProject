b09b48c3c86e1975eb9213dbd5158cd49e0d04b4
negar <negar@guest-wireless-upc-1602-10-120-033-049.usc.edu>
2017-04-25 19:41:47 -0700

    energy calculation

--- src/EnergyCalculater.java
+++ src/EnergyCalculater.java
@@ -10,7 +10,7 @@ public class EnergyCalculater {
 		String energyFile = "energy.txt";
 		// This will reference one line at a time
 		String line = null;
-
+		String result = "";
 		try {
 			// FileReader reads text files in the default encoding.
 			FileReader fileReader = new FileReader(logFile);
@@ -18,15 +18,24 @@ public class EnergyCalculater {
 			// Always wrap FileReader in BufferedReader.
 			BufferedReader bufferedReader = new BufferedReader(fileReader);
 			BufferedReader bufferedReader2 = new BufferedReader(fileReader2);
-			String unixTime = findInFile(bufferedReader2);
+			String unixTime = findInFile(bufferedReader2, "unixtime").replaceAll(" ", "").split(":")[1];
+			long unixtime = Long.parseLong(unixTime);
+			String preLine = "";
 			while ((line = bufferedReader.readLine()) != null) {
-				line = line.replaceAll("<", "");
-				line = line.replaceAll(" ", "");
-				line = line.replaceAll(">", " ");
-				String elements[] = line.split(" ");
+				long timeMili = getTime(line, unixtime);
+				long preTimeMili = getTime(preLine, unixtime);
+
+				if (line.contains("add")) {
+					result += "add: time = " + timeMili + " values = ";
+					result += findInFile(bufferedReader2, "#" + timeMili);
+					result += " pre time = " + preTimeMili + " values = ";
+					result += findInFile(bufferedReader2, "#" + preTimeMili);
+				}
+
+				preLine = line;
 				
 			}
-
+			
 			// Always close files.
 			bufferedReader.close();
 			bufferedReader2.close();
@@ -37,15 +46,37 @@ public class EnergyCalculater {
 			// Or we could just do this:
 			// ex.printStackTrace();
 		}
+		System.out.println(result);
 	}
 
-	private static String findInFile(BufferedReader bufferedReader2) {
+	private static long getTime(String line, long unixtime) {
+		if (line == "") {
+			return 0;
+		}
+		line = line.replaceAll("<", "");
+		line = line.replaceAll(" ", "");
+		line = line.replaceAll(">", " ");
+		String elements[] = line.split(" ");
+		String timedone = elements[1].split(":")[1];
+		return Long.parseLong(timedone) - unixtime;
+	}
+
+	private static String findInFile(BufferedReader bufferedReader2, String str) {
 		String line = "";
+		String target = str;
+		if (str.startsWith("#")) {
+			target = str.substring(1);
+		}
 		try {
-			while((line = bufferedReader2.readLine()) != line){
-				if(line.contains("unixtime")){
-					return line;
+			while ((line = bufferedReader2.readLine()) != line) {
+				if (line.contains(target)) {
+					if (!str.startsWith("#"))
+						return line;
+					else if(line.startsWith("#")){
+						return line.split(" ")[3] + " "+line.split(" ")[5];
+					}
 				}
+
 			}
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
